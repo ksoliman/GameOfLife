18dfabf68bec554517215eb739fed98740cb0bc7
ksoliman <ksoliman1994@gmail.com>
2019-05-10 17:10:03 -0400

    Update gol.cpp

--- gol.cpp
+++ gol.cpp
@@ -1,16 +1,5 @@
 /*
  * CSc103 Project 3: Game of Life
- * See readme.html for details.
- * Please list all references you made use of in order to complete the
- * assignment: your classmates, websites, etc.  Aside from the lecture notes
- * and the book, please list everything.  And remember- citing a source does
- * NOT mean it is okay to COPY THAT SOURCE.  What you submit here **MUST BE
- * YOUR OWN WORK**.
- * References:
- *
- *
- * Finally, please indicate approximately how many hours you spent on this:
- * #hours: 
  */
 #include<iostream>
 #include <cstdio>
